a8bd41283f18d9c2f6613ca9fb804f7b2aaf060e
KurniadiY <68764207+KurniadiY@users.noreply.github.com>
2020-08-29 20:56:27 +0700

    first commit

---
+++ Variable.py
@@ -0,0 +1,12 @@
+# %%
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import sklearn
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+
+
+# %%
